lustre/e2fsprogs

LU-6722 jbd: double minimum journal size for RHEL7
In RHEL7 (kernel version >= 3.10.0), the maximum journal transaction
size is reduced to half of the RHEL6 case. That may cause Lustre to
complain that the declared transaction credits too large for very
small journal device. This patch increases the minimum journal size
from 4MB to 8MB for RHEL7 case, then counteract the above limitation
about the journal transaction size.

Signed-off-by: Fan Yong <fan.yong@intel.com>
Change-Id: Iec8a2c561416cb7b5acce342c8ebcb845c8d7a19
Reviewed-on: http://review.whamcloud.com/15401
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Niu Yawei <yawei.niu@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

misc/util.c
@@ -436,13 +436,20 @@ unsigned int figure_journal_size(int size, ext2_filsys fs)
 	}
 
 	if (size > 0) {
+		int min_size;
+
+		if (ext2fs_is_before_linux_ver(3, 10, 0) ||
+		    ext2fs_blocks_count(fs->super) <= 8192)
+			min_size = 1024;
+		else
+			min_size = 2048;
+
 		j_blocks = size * 1024 / (fs->blocksize	/ 1024);
-		if (j_blocks < 1024 || j_blocks > 10240000) {
+		if (j_blocks < min_size || j_blocks > 10240000) {
 			fprintf(stderr, _("\nThe requested journal "
 				"size is %d blocks; it must be\n"
-				"between 1024 and 10240000 blocks.  "
-				"Aborting.\n"),
-				j_blocks);
+				"between %d and 10240000 blocks.  "
+				"Aborting.\n"), j_blocks, min_size);
 			exit(1);
 		}
 		if ((unsigned) j_blocks > ext2fs_free_blocks_count(fs->super) / 2) {

dongeon97/cfsck

misc/util.c
@@ -49,6 +49,7 @@
 #include "e2p/e2p.h"
 #include "ext2fs/ext2_fs.h"
 #include "ext2fs/ext2fs.h"
+#include "ext2fs/ext2fsP.h"
 #include "support/nls-enable.h"
 #include "support/devname.h"
 #include "blkid/blkid.h"
@@ -276,6 +277,14 @@ void figure_journal_size(struct ext2fs_journal_params *jparams,
 
 	if (requested_j_size > 0 ||
 		(ext2fs_has_feature_fast_commit(fs->super) && requested_fc_size > 0)) {
+		int min_size;
+
+		if (ext2fs_is_before_linux_ver(3, 10, 0) ||
+		    ext2fs_blocks_count(fs->super) <= 8192)
+			min_size = 1024;
+		else
+			min_size = 2048;
+
 		if (requested_j_size > 0)
 			jparams->num_journal_blocks =
 				jsize_to_blks(fs, requested_j_size);
@@ -286,12 +295,12 @@ void figure_journal_size(struct ext2fs_journal_params *jparams,
 		else if (!ext2fs_has_feature_fast_commit(fs->super))
 			jparams->num_fc_blocks = 0;
 		total_blocks = jparams->num_journal_blocks + jparams->num_fc_blocks;
-		if (total_blocks < 1024 || total_blocks > 10240000) {
+		if (total_blocks < min_size || total_blocks > 10240000) {
 			fprintf(stderr, _("\nThe total requested journal "
 				"size is %d blocks; it must be\n"
-				"between 1024 and 10240000 blocks.  "
+				"between %d and 10240000 blocks.  "
 				"Aborting.\n"),
-				total_blocks);
+				total_blocks, min_size);
 			exit(1);
 		}
 		if ((unsigned int) total_blocks > ext2fs_free_blocks_count(fs->super) / 2) {